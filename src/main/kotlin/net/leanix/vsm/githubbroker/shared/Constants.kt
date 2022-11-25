40b639cc5184d2463958dc9b7370af8d37ad1203
Henrique E. do Amaral <hen.amaral@gmail.com>
2022-11-25 15:54:48 +0100

    CID-1091 Change connector name

--- src/main/kotlin/net/leanix/vsm/githubbroker/shared/Constants.kt
+++ src/main/kotlin/net/leanix/vsm/githubbroker/shared/Constants.kt
@@ -2,5 +2,5 @@ package net.leanix.vsm.githubbroker.shared
 
 object Constants {
 
-    const val GITHUB_ENTERPRISE_CONNECTOR = "github-on-prem-repository-connector"
+    const val GITHUB_ENTERPRISE_CONNECTOR = "github-enterprise-repository-connector"
 }
