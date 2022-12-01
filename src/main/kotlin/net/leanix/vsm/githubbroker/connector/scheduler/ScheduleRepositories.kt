07cebfbba0410a3389b373d3c21be0aa4a47043f
Henrique E. do Amaral <hen.amaral@gmail.com>
2022-12-01 15:26:57 +0100

    CID-1109 Add schedule

---
+++ src/main/kotlin/net/leanix/vsm/githubbroker/connector/scheduler/ScheduleRepositories.kt
@@ -0,0 +1,22 @@
+package net.leanix.vsm.githubbroker.connector.scheduler
+
+import net.leanix.vsm.githubbroker.connector.application.RepositoriesService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+
+@Component
+class ScheduleRepositories(private val repositoriesService: RepositoriesService) {
+    private val logger: Logger = LoggerFactory.getLogger(ScheduleRepositories::class.java)
+
+    @Scheduled(cron = "\${leanix.vsm.schedule:0 0 16 * * *}")
+    fun getAllRepositories() {
+        kotlin.runCatching {
+            logger.info("Started schedule")
+            repositoriesService.getAllRepositories()
+        }.onFailure {
+            logger.error("Schedule failed", it)
+        }
+    }
+}
