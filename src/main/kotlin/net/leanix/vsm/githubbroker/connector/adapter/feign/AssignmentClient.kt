1dbe041d939a1805d2a4a2228ee3fc87584899d1
George Andrinopoulos <george.andrinopoulos@leanix.net>
2023-01-18 11:02:29 +0200

    Initial commit

--- src/main/kotlin/net/leanix/vsm/githubbroker/connector/adapter/feign/AssignmentClient.kt
+++ src/main/kotlin/net/leanix/vsm/githubbroker/connector/adapter/feign/AssignmentClient.kt
@@ -1,7 +1,7 @@
 package net.leanix.vsm.githubbroker.connector.adapter.feign
 
 import net.leanix.vsm.githubbroker.connector.adapter.feign.data.AssignmentResponse
-import net.leanix.vsm.githubbroker.shared.auth.adapter.feign.config.FeignClientConfiguration
+import net.leanix.vsm.githubbroker.shared.auth.adapter.feign.config.MtmFeignClientConfiguration
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam
 @FeignClient(
     name = "assignmentClient",
     url = "\${leanix.vsm.base-url}",
-    configuration = [FeignClientConfiguration::class]
+    configuration = [MtmFeignClientConfiguration::class]
 )
 interface AssignmentClient {
 
--- src/main/kotlin/net/leanix/vsm/githubbroker/shared/exception/VsmException.kt
+++ src/main/kotlin/net/leanix/vsm/githubbroker/shared/exception/VsmException.kt
@@ -3,4 +3,6 @@ package net.leanix.vsm.githubbroker.shared.exception
 sealed class VsmException(message: String? = null) : RuntimeException(message) {
 
     class NoRepositoriesFound : VsmException()
+
+    class WebhookRegistrationFailed(message: String) : VsmException(message)
 }
