a708099374d1b51320a93ebd8024115515235066
Henrique E. do Amaral <hen.amaral@gmail.com>
2023-01-19 10:35:52 +0100

    CID-1157: Add GH DTO's

---
+++ src/main/kotlin/net/leanix/vsm/githubbroker/connector/adapter/json/data/RepositoryData.kt
@@ -0,0 +1,15 @@
+package net.leanix.vsm.githubbroker.connector.adapter.json.data
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class RepositoryData(
+    @JsonProperty("node_id")
+    val id: String,
+    val name: String,
+    val archived: Boolean,
+    val description: String?,
+    val url: String,
+    val visibility: String,
+    val topics: List<String>?,
+    val language: String?
+)
