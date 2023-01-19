d3e6dc38ba83f2f8e15d14b810b4dcd42a3f4a73
Henrique E. do Amaral <hen.amaral@gmail.com>
2023-01-19 17:12:50 +0100

    CID-1157: Fix detekt issues

--- src/main/kotlin/net/leanix/vsm/githubbroker/connector/adapter/json/data/RepositoryPayload.kt
+++ src/main/kotlin/net/leanix/vsm/githubbroker/connector/adapter/json/data/RepositoryPayload.kt
@@ -3,4 +3,4 @@ package net.leanix.vsm.githubbroker.connector.adapter.json.data
 data class RepositoryPayload(
     override var action: String,
     val repository: RepositoryData
-) : BaseGitHubPayload()
+) : BaseGitHubPayload
