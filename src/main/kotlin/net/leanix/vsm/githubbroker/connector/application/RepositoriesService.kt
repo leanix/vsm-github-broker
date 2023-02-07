2af220076318ed21d597a1598c57a978f9596828
Henrique E. do Amaral <hen.amaral@gmail.com>
2023-02-07 12:36:02 +0100

    CID-1157: Add start and end log messages for fetch repos

--- src/main/kotlin/net/leanix/vsm/githubbroker/connector/application/RepositoriesService.kt
+++ src/main/kotlin/net/leanix/vsm/githubbroker/connector/application/RepositoriesService.kt
@@ -2,6 +2,7 @@ package net.leanix.vsm.githubbroker.connector.application
 
 import net.leanix.vsm.githubbroker.connector.domain.Assignment
 import net.leanix.vsm.githubbroker.connector.domain.GithubRepositoryProvider
+import net.leanix.vsm.githubbroker.logs.domain.LogStatus
 import net.leanix.vsm.githubbroker.shared.exception.VsmException
 import org.springframework.stereotype.Service
 
@@ -12,8 +13,9 @@ class RepositoriesService(
 ) : BaseConnectorService() {
 
     fun getAllRepositories(assignment: Assignment) {
-        logInfoStatus(runId = assignment.runId)
+        logInfoStatus(runId = assignment.runId, status = LogStatus.IN_PROGRESS)
         getRepositoriesPaginated(assignment)
+        logInfoStatus(runId = assignment.runId, status = LogStatus.SUCCESSFUL)
     }
 
     private fun getRepositoriesPaginated(assignment: Assignment) {
