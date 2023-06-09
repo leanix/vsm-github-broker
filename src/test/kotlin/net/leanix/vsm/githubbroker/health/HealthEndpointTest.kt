0c5146bf0e3aa2c714eb88c52f210df3623eed4c
George Andrinopoulos <george.andrinopoulos@leanix.net>
2023-06-09 09:40:12 +0300

    CID-1650 Expose actuator health endpoint

--- src/test/kotlin/net/leanix/vsm/githubbroker/health/HealthEndpointTest.kt
+++ src/test/kotlin/net/leanix/vsm/githubbroker/health/HealthEndpointTest.kt
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock
-import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
